e99eccf7d4e94b1cf2a2669d77963e76617bafb4
loex345 <maitlandeg@gmail.com>
2022-08-31 12:57:02 -0700

    Mern stack clean-up

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -49,7 +49,3 @@ export function getUser() {
   return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
 
-export function checkToken() {
-  return usersAPI.checkToken()
-    .then(dateStr => new Date(dateStr));
-}
